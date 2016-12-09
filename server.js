0f2aa86598a8014dbc0b129ec611ae58bf06a1d0
andimarek <andimarek@fastmail.fm>
2016-12-09 16:30:24 +0100

    test green again

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hellx World");
+  response.end("Hello World");
 });
 
 server.listen(8080);
